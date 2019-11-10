471c0d61902db13c0355c5ba339bdaea5b9c87b3
JayJayTing <tingjayj@gmail.com>
2019-11-10 05:54:58 -0500

    Unity init commit

---
+++ Assets/Scenes/Introducer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introducer : MonoBehaviour {
+
+    public int movementSpeed;
+	// Use this for initialization
+	void Start () {
+        print("hello there");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
